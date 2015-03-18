67aed25675440f43b089b6ac89c01c8a5755c9df
gabe rankin <gabe.rankin@gmail.com>
2015-03-18 12:14:12 -0400

    origin correction on experiment

--- lab/midpoint-stackoverflow.js
+++ lab/midpoint-stackoverflow.js
@@ -2,7 +2,7 @@
  *	Porting code described from http://stackoverflow.com/questions/10878209/midpoint-circle-algorithm-for-filled-circles
  *	to javascript.  this is to be used to test speeds of different drawing algorithms.
  */
-var canvas = null, context = null, imgData = null, data = null;
+var canvas = null, context = null, imgData = null, data = null, centerX, centerY, radius;
 function init() {
 	canvas = document.getElementById('canvas');
 	context = canvas.getContext('2d');
@@ -26,54 +26,60 @@ function init() {
 function draw() {
 	console.time('draw');
 
-	var centerX = 129,
-		centerY = 129,
-		radius = 128;
+	centerX = Math.round(canvas.width / 2),
+	centerY = Math.round(canvas.height / 2),
+	radius = 128;
+	
 	var x = radius,
 		y = 0,
 		radiusError = 1 - x;
 	var startX, endX
 
-    while (x >= y)  // iterate to the circle diagonal
-    {
+	var drawCircle = function() {
+		if (x >= y)  // iterate to the circle diagonal
+		{
 
-        // use symmetry to draw the two horizontal lines at this Y with a special case to draw
-        // only one line at the centerY where y == 0
-        startX = -x + centerX;
-        endX = x + centerX;         
-        drawHorizontalLine( startX, endX, y + centerY );
-        if (y != 0)
-        {
-            drawHorizontalLine( startX, endX, -y + centerY );
-        }
+			// use symmetry to draw the two horizontal lines at this Y with a special case to draw
+			// only one line at the centerY where y == 0
+			startX = -x + centerX;
+			endX = x + centerX;         
+			drawHorizontalLine( startX, endX, y + centerY );
+			if (y != 0)
+			{
+				drawHorizontalLine( startX, endX, -y + centerY );
+			}
 
-        // move Y one line
-        y++;
+			// move Y one line
+			y++;
 
-        // calculate or maintain new x
-        if (radiusError<0)
-        {
-            radiusError += 2 * y + 1;
-        } 
-        else 
-        {
-            // we're about to move x over one, this means we completed a column of X values, use
-            // symmetry to draw those complete columns as horizontal lines at the top and bottom of the circle
-            // beyond the diagonal of the main loop
-            if (x >= y)
-            {
-                startX = -y + 1 + centerX;
-                endX = y - 1 + centerX;
-                drawHorizontalLine( startX, endX,  x + centerY );
-                drawHorizontalLine( startX, endX, -x + centerY );
-            }
-            x--;
-            radiusError += 2 * (y - x + 1);
-        }
+			// calculate or maintain new x
+			if (radiusError<0)
+			{
+				radiusError += 2 * y + 1;
+			} 
+			else 
+			{
+				// we're about to move x over one, this means we completed a column of X values, use
+				// symmetry to draw those complete columns as horizontal lines at the top and bottom of the circle
+				// beyond the diagonal of the main loop
+				if (x >= y)
+				{
+					startX = -y + 1 + centerX;
+					endX = y - 1 + centerX;
+					drawHorizontalLine( startX, endX,  x + centerY );
+					drawHorizontalLine( startX, endX, -x + centerY );
+				}
+				x--;
+				radiusError += 2 * (y - x + 1);
+			}
+			setTimeout(drawCircle, 0);
+			return;
+		}
+		console.timeEnd('draw');
+	}
+	setTimeout(drawCircle, 0);
 
-    }
 
-	console.timeEnd('draw');
 }
 
 
@@ -83,10 +89,11 @@ function drawHorizontalLine(startX, endX, y) {
 		startX = endX;
 		endX = tmp;
 	}
-	var dist, angle, h, l;
+	var dist, angle, h, l, correctedX, correctedY = y - centerY;
 	for(var x = startX; x <= endX; x++) {
-		dist = Math.sqrt(x*x + y*y);
-		angle = Math.atan2(y, x) * 180 / Math.PI;
+		correctedX = x - centerX;
+		dist = Math.sqrt(correctedX*correctedX + correctedY*correctedY);
+		angle = Math.atan2(correctedY, correctedX) * 180 / Math.PI;
 		h = angle / 360;
 		l = 1 - (dist / 128);
 		rgb = Col.hslToRgb(h,1,l);
